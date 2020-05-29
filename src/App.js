ef96e95e48cd9feb6e0447903583183e565ae747
ilia <ilyakut@gmail.com>
2020-05-29 22:14:12 +0300

    added link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import First from "./components/first";
 import Second from "./components/second";
 
@@ -11,6 +11,8 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <Link to="/first">First</Link>
+          <Link to="/second">Second</Link>
           <Route path="/first" component={First}/>
           <Route path="/second" component={Second}/>
         </header>
