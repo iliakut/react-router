967ecc81f2d1a86c769b64efc7cd280fd8fb7aec
ilia <ilyakut@gmail.com>
2020-05-30 12:24:14 +0300

    added exact route

--- src/App.js
+++ src/App.js
@@ -11,10 +11,19 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <Link to="/">Main</Link>
           <Link to="/first">First</Link>
           <Link to="/second">Second</Link>
+          <Route path="/" exact render={() => <h1>React router test</h1>}/>
           <Route path="/first" component={First}/>
           <Route path="/second" component={Second}/>
+          {/*
+          Route
+          path - путь соответствия (содержит путь) "/" - содержится во всех роутах, поэтому нужен exact
+          exact - тогда роут будет сравниваться полностью
+          component - комопнент, который будет показан
+          render - может принять сразу JSX для отображения
+          */}
         </header>
       </div>
     </Router>
